e93bad6747eeaf222e9c5faad238c2ba5d36665c
Diego Pereira <diegopereiraeng@gmail.com>
2021-05-13 21:14:09 -0400

    Changed From Payload to plugin parameters

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ ACCOUNT_ID = os.environ.get('PLUGIN_ACCOUNTID')
 API_KEY = os.environ.get('PLUGIN_APIKEY')
 APPLICATION_NAME = os.environ.get('PLUGIN_APPLICATION')
 WORKFLOW_NAME = os.environ.get('PLUGIN_ENTITYNAME')
-BODY = os.environ.get('PLUGIN_BODY')
+#BODY = os.environ.get('PLUGIN_BODY')
 EXECUTION_TYPE = os.environ.get('PLUGIN_TYPE')
+SERVICE_NAME = os.environ.get('PLUGIN_SERVICE_NAME')
+BUILD_NUMBER = os.environ.get('PLUGIN_BUILD_NUMBER')
+ARTIFACT_SOURCE_NAME = os.environ.get('PLUGIN_ARTIFACT_SOURCE_NAME')
 
 global URL
 URL = "https://app.harness.io/gateway/api/graphql?accountId=" + ACCOUNT_ID
@@ -65,11 +68,35 @@ def getPLByName(AppID, PLName):
     return(PLID)
 
 def execute(appID, wfID):
+    print("Service Name: " + SERVICE_NAME)
+    print("Build Number: " + BUILD_NUMBER)
+    print("Artifact Source Name: " + ARTIFACT_SOURCE_NAME)
+    print("Execution Type: " + EXECUTION_TYPE)
+    body = 'variableInputs: [ \
+      {\
+        name: "Service"\
+        variableValue: {\
+          type: NAME\
+          value: "'+SERVICE_NAME+'"\
+        }\
+      }\
+      ], \
+      serviceInputs: [ {\
+        name: "'+SERVICE_NAME+'", \
+        artifactValueInput: {\
+          valueType: BUILD_NUMBER\
+          buildNumber: {\
+            buildNumber: "'+BUILD_NUMBER+'"\
+      artifactSourceName: "'+ARTIFACT_SOURCE_NAME+'"\
+          }\
+        }}  ]'
+      
+    
     pload = "mutation { \
               startExecution(input: { \
                 applicationId: \"" + appID + "\" \
                 entityId: \"" + wfID + "\" \
-                executionType: " + EXECUTION_TYPE + ", " + BODY + \
+                executionType: " + EXECUTION_TYPE + ", " + body + \
               "} \
               ){ \
                 clientMutationId \
@@ -79,7 +106,7 @@ def execute(appID, wfID):
                 } \
               } \
             }"
-
+    #print("Payload: " + pload)
     response = requests.post(URL, headers={'x-api-key': API_KEY}, data=pload)
     json_response = response.json()
     print(json_response)
